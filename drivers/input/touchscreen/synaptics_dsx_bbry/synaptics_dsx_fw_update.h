f9faed4580007ef51305dbc76f59af2ee1cdbeae
dlegault <dlegault@blackberry.com>
2016-02-04 09:01:21 -0500

    Kernel Source Code for MSM8992 AAD447 Image

---
+++ drivers/input/touchscreen/synaptics_dsx_bbry/synaptics_dsx_fw_update.h
@@ -0,0 +1,21 @@
+/*
+ * Synaptics DSX touchscreen driver
+ *
+ * Copyright (C) 2014  BlackBerry Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _SYNAPTICS_DSX_FW_UPDATE_H_
+#define _SYNAPTICS_DSX_FW_UPDATE_H_
+
+int synaptics_rmi4_fw_update_module_init(struct synaptics_rmi4_data *rmi4_data);
+
+#endif
