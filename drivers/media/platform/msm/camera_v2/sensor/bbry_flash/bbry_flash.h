c8c8e602599e99d97acc2ae898895b3ed0705d49
ci_dev_builds <jenkins@blackberry.com>
2017-02-03 14:52:13 -0500

    Kernel Source Code for MSM8992 AAI459 Image

---
+++ drivers/media/platform/msm/camera_v2/sensor/bbry_flash/bbry_flash.h
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2015 BlackBerry Limited
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __BBRY_FLASH_H__
+#define __BBRY_FLASH_H__
+
+#include <linux/platform_device.h>
+#include <media/msm_cam_sensor.h>
+#include <linux/leds.h>
+#include <soc/qcom/camera2.h>
+#include "msm_sd.h"
+
+#define MAX_DRIVER_NAME_LENGTH	32
+
+struct bbry_flash_device_t;
+struct bbry_flash_ctrl_t;
+
+enum bbry_flash_device_type_t {
+	PMI8994_FLASH,
+	MAX77387_FLASH,
+	INVALID_FLASH
+};
+
+struct bbry_flash_cmd_t {
+	enum msm_flash_cfg_type_t cfg_type;
+	uint32_t requested_current;
+	uint32_t limited_current;
+	uint32_t applied_current;
+	int32_t flash_duration;
+};
+
+struct bbry_flash_driver_t {
+	enum bbry_flash_device_type_t type;
+	char name[MAX_DRIVER_NAME_LENGTH];
+	int32_t (*probe)(struct bbry_flash_ctrl_t *, struct device_node *,
+			struct device_node *, struct bbry_flash_device_t *);
+};
+
+struct bbry_flash_func_t {
+	int32_t (*flash_init)(struct bbry_flash_device_t *,
+				struct bbry_flash_cmd_t *);
+	int32_t (*flash_release)(struct bbry_flash_device_t *,
+				struct bbry_flash_cmd_t *);
+	int32_t (*flash_off)(struct bbry_flash_device_t *,
+				struct bbry_flash_cmd_t *);
+	int32_t (*torch_on)(struct bbry_flash_device_t *,
+				struct bbry_flash_cmd_t *);
+	int32_t (*flash_low)(struct bbry_flash_device_t *,
+				struct bbry_flash_cmd_t *);
+	int32_t (*flash_high)(struct bbry_flash_device_t *,
+				struct bbry_flash_cmd_t *);
+#ifdef CONFIG_BBRY_MFG
+	int32_t (*flash_status)(struct bbry_flash_device_t *,
+				struct bbry_flash_cmd_t *);
+#endif /*ifdef CONFIG_BBRY_MFG */
+};
+
+struct bbry_flash_mode_t {
+	const char *trigger_name;
+	struct led_trigger *trigger;
+	uint32_t max_current;
+	uint32_t default_current;
+};
+
+struct bbry_flash_mitigation_t {
+	uint32_t *levels;
+	uint32_t num_levels;
+	uint32_t limit;
+};
+
+struct bbry_flux_curve_t {
+	uint32_t *mA_values;
+	uint32_t *rel_flux_values;
+	uint32_t num_points;
+};
+
+struct bbry_flash_device_t {
+	enum bbry_flash_device_type_t type;
+	struct bbry_flash_func_t func_tbl;
+
+	struct bbry_flash_mode_t flash;
+	struct bbry_flash_mode_t torch;
+
+	struct bbry_flash_cmd_t state;
+};
+
+struct bbry_flash_ctrl_t {
+	struct platform_device *pdev;
+	struct msm_sd_subdev msm_sd;
+	struct msm_camera_power_ctrl_t power_info;
+	struct pinctrl *pinctrl;
+	struct pinctrl_state *gpio_state_default;
+	struct pinctrl_state *gpio_state_suspend;
+
+	uint32_t high_max_current;	/* max current in flash_high mode */
+	uint32_t low_max_current;	/* max current in flash_low mode */
+	uint32_t torch_max_current;	/* max current in torch_on mode */
+	bool use_fixed_ratio;
+	bool keep_alive;
+	uint32_t preflash_divisor;
+	struct bbry_flash_mitigation_t flash_mitigation;
+	struct bbry_flash_mitigation_t torch_mitigation;
+	struct bbry_flux_curve_t flux_curve;
+
+	struct msm_flash_hw_data_t hw_data;
+	struct bbry_flash_device_t flash[MAX_LED_TRIGGERS];
+	struct regulator *vdd;
+	bool vdd_enabled;
+	enum msm_flash_cfg_type_t state;
+};
+
+#endif /* __BBRY_FLASH_H__ */
---
+++ drivers/net/wireless/bcmdhd_1_201_31_angler/wlan_ddt/wlan_ddt.c
@@ -0,0 +1,94 @@
+/*
+ * WLAN driver DDT manager
+ *
+ * Copyright (C) 2015 BlackBerry Limited
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/version.h>
+#include <misc/ddt.h>
+#include "wlan_ddt.h"
+
+/* QUIP event creator name string */
+#define CREATOR_NAME    "wlan_driver"
+
+/**
+ * QUIP event details lookup table
+ *
+ * Legend for temp investigation events:
+ *   d1 & d2 - unique identifier for WLAN, must be 87 and 73
+ *   d3      - must be 1 (the value 0 is used by WifiService in Java)
+ *   d4      - an incremental value used to identify the particular temp event
+ **/
+struct logworthy_event_details_t wlan_drv_ddt_event_details[] = {
+	[WLAN_DRV_EVENT_TEST] = {87, 73, 1, 99, CREATOR_NAME},
+	[WLAN_DRV_EVENT_PCIE_LINKDOWN] = {1, 0, 0, 0, CREATOR_NAME},
+	[WLAN_DRV_EVENT_MAX] = {0, 0, 0, 0, CREATOR_NAME}, /* Always last */
+};
+
+static char *wlan_ddt_event_name(wlan_drv_event_t event_type)
+{
+	switch (event_type) {
+	case WLAN_DRV_EVENT_TEST:
+		return "WLAN DRIVER TEST EVENT";
+	case WLAN_DRV_EVENT_PCIE_LINKDOWN:
+		return "PCIE LINKDOWN EVENT";
+	default:
+		break;
+	}
+	return NULL;
+}
+
+int wlan_ddt_event_send(wlan_drv_event_t event_type)
+{
+	struct logworthy_event_details_t *details;
+	int ddt_event;
+	int ret;
+	char *event_name;
+
+	/* convert WLAN event type to DDT event */
+	switch (event_type) {
+	case WLAN_DRV_EVENT_TEST:
+		ddt_event = LW_EVENT_TEST_00000103;
+		break;
+	case WLAN_DRV_EVENT_PCIE_LINKDOWN:
+		ddt_event = LW_EVENT_WLAN_00000900;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	/* Get event_name */
+	event_name = wlan_ddt_event_name(event_type);
+	if (event_name == NULL) {
+		pr_err("%s: Invalid WLAN driver event\n", __func__);
+		return -EINVAL;
+	}
+
+	/* Assign the event details */
+	details = &wlan_drv_ddt_event_details[event_type];
+
+	/* Send to DDT */
+	ret = ddt_send(ddt_event, details,
+			"['logcat://main','dumpsys://wifi','kerneldump://']");
+	if (ret < 0) {
+		pr_err("%s: Failed to create \"%s\" event (err::%d)\n",
+			__func__, event_name, ret);
+	} else {
+		pr_err("%s: Successfully created \"%s\" event\n",
+			__func__, event_name);
+	}
+
+	return ret;
+}
+
