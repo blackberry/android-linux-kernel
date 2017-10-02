02659777cc66ac110cd5e155739dd9bb1ddd3dc7
ci_dev_builds <jenkins@blackberry.com>
2017-10-02 09:10:38 -0400

    Kernel Source Code for MSM8992 AAO474 Image

---
+++ include/misc/lw_event_types.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 BlackBerry Limited
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef LW_EVENT_TYPES_H_
+#define LW_EVENT_TYPES_H_
+
+/**
+ * DDT Logworthy Event Types
+ *
+ */
+
+#define LW_EVENT_MTOUCH_0000059a               0x0000059a
+#define LW_EVENT_MTOUCH_0000059b               0x0000059b
+#define LW_EVENT_MTOUCH_0000059c               0x0000059c
+#define LW_EVENT_MTOUCH_000005aa               0x000005aa
+#define LW_EVENT_MTOUCH_00002520               0x00002520
+#define LW_EVENT_MTOUCH_00002521               0x00002521
+#define LW_EVENT_MTOUCH_00002522               0x00002522
+#define LW_EVENT_MTOUCH_00002523               0x00002523
+#define LW_EVENT_KEYPAD_00002525               0x00002525
+#define LW_EVENT_KEYPAD_00002526               0x00002526
+#define LW_EVENT_KEYPAD_00002527               0x00002527
+#define LW_EVENT_DISPLAY_00002530              0x00002530
+#define LW_EVENT_PCIE_AER_00002541             0x00002541
+#define LW_EVENT_PCIE_AER_00002542             0x00002542
+#define LW_EVENT_PCIE_AER_00002543             0x00002543
+
+#define LW_EVENT_WLAN_00000900                 0x00000900
+
+/* Generic for testing event creation */
+#define LW_EVENT_TEST_00000103                 0x00000103
+
+#endif /* LW_EVENT_TYPES_H_ */
